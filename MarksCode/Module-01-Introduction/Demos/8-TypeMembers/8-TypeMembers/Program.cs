8d86a3dbff7709d53ea5897be06b3ba68d4e9625
Student@DDLS <Student@DDLS>
2022-06-28 22:18:39 -0700

    Added demo 8 for m1

---
+++ MarksCode/Module-01-Introduction/Demos/8-TypeMembers/8-TypeMembers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_TypeMembers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 44;
+            Console.WriteLine(x.ToString());
+
+            string y;
+            Console.WriteLine("Enter a number");
+            y = Console.ReadLine();
+
+            //OK
+            //Console.WriteLine($"{y} divided by 3 is {Convert.ToInt32(y)/3}");
+
+            //Better
+            if (int.TryParse(y, out x))
+            {
+                Console.WriteLine($"{x} divided by 3 is {Convert.ToInt32(x) / 3}");
+            }
+            else
+            {
+                Console.WriteLine($"{y} is not an integer");
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
+                    
