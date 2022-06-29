bdfeae5f49205b712c71280957604db8bb981eca
Student@DDLS <Student@DDLS>
2022-06-28 21:49:41 -0700

    Add m1 examples

---
+++ MarksCode/Module-01-Introduction/Demos/7-DataTypes/ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.Services;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int price;
+            price = 0;
+            Console.WriteLine($"price is {price}");
+
+            Console.WriteLine($"The largest integer is {int.MaxValue}");
+            Console.WriteLine($"The largest long is {long.MaxValue}");
+            Console.WriteLine($"The largest float is {float.MaxValue}");
+            Console.WriteLine($"The largest double is {double.MaxValue}");
+            Console.WriteLine($"The largest decimal is {decimal.MaxValue}");
+
+
+            Console.WriteLine(DateTime.Now.Hour>12?"It's afternoon":"It's Morning");
+
+            StringBuilder sb = new StringBuilder();
+
+            int x = new int();
+            
+            Console.ReadLine();
+        }
+    }
+}
