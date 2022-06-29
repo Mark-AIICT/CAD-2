40bf6b1114569c392d52e78a469d0aad5bcf67cd
Student@DDLS <Student@DDLS>
2022-06-28 18:24:02 -0700

    Add demos for M1

---
+++ MarksCode/Module-01-Introduction/Demos/00-SimpleForEach/00-SimpleForEach/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _00_SimpleForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] foods = new string[] { "Cake", "Sudza", "Kiribath" };
+
+            for (int i = 0; i < foods.Length; i++)
+            {
+                Console.WriteLine(foods[i]);
+            }
+
+            global::System.Console.WriteLine("Press any key to finish");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MarksCode/Module-01-Introduction/Demos/6-Recursion/6-Recursion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long value = Factorial(10);
+            Console.Out.WriteLine(value);
+            
+        }
+        static long Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            return n * Factorial(n - 1);
+        }
+    }
+}
