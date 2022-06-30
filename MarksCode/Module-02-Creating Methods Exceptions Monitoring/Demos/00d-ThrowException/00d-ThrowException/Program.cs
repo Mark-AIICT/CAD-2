726c494f113e303cbae260c02392a75cc5d3c823
Student@DDLS <Student@DDLS>
2022-06-29 21:22:40 -0700

    Throwing exceptions

---
+++ MarksCode/Module-02-Creating Methods Exceptions Monitoring/Demos/00d-ThrowException/00d-ThrowException/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00d_ThrowException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string invoice;
+                invoice = ConstructInvoice(3, -6);
+                Console.WriteLine(invoice);
+            }
+            catch (ApplicationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+
+        static string ConstructInvoice(decimal qty, decimal price)
+        {
+
+            decimal invoiceTotal = qty * price;
+
+            //Check the invoice total
+            if (invoiceTotal <= 0)
+                throw new ApplicationException("Invoice total is invalid. Must be >= 0");
+
+            return $"invoice: Total Amount {invoiceTotal:C}";
+        }
+    }
+}
