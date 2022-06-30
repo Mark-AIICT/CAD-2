193eee4b8596bcc5d3d0588939a7b5c80da48746
Student@DDLS <Student@DDLS>
2022-06-29 18:58:47 -0700

    add overloaded

---
+++ MarksCode/Module-02-Creating Methods Exceptions Monitoring/Demos/00a-OverloadedMethods/00a-OverloadedMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00a_OverloadedMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"The cube of 4 is " + Cube(4));
+            Console.WriteLine($"The cube of 5 is " + Cube("5"));
+            Console.ReadLine();
+
+        }
+
+        static int Cube(int x)
+        {
+            return x * x * x;
+        }
+
+        static int Cube(string x)
+        {
+            int z = Convert.ToInt32(x);
+            return z * z * z;
+        }
+    }
+}
