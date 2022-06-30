293a14b05bf59087d2855665b7155bef29feb793
Student@DDLS <Student@DDLS>
2022-06-29 18:50:16 -0700

    Add Methods Demo to m2

---
+++ MarksCode/Module-02-Creating Methods Exceptions Monitoring/Demos/00-Methods/00-Methods/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y = 9;
+            int result = Cube(y);
+            Console.WriteLine($"The cube of {y} is {result} ");
+            Console.ReadLine();
+
+        }
+
+        static int Cube(int x)
+        {
+            return x * Square(x);
+        }
+
+        static int Square(int x)
+        {
+            return x * x;
+        }
+
+
+    }
+}
