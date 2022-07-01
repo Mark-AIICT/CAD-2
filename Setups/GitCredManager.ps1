142b6e6e1beae0240556ed6d1226ee5072f274c1
Student@DDLS <Student@DDLS>
2022-06-30 17:46:24 -0700

    Change Trigger for Scheduled task

--- Setups/GitCredManager.ps1
+++ Setups/GitCredManager.ps1
@@ -1,6 +1,6 @@
 $Action = New-ScheduledTaskAction -Execute 'cmd' -Argument '/C git credential-manager-core configure' 
-$Trigger = New-ScheduledTaskTrigger -AtStartup
+$Trigger = New-ScheduledTaskTrigger -AtLogOn
 $Settings = New-ScheduledTaskSettingsSet
 $Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Settings
-$principal=new-scheduledtaskprincipal -userid 'NT AUTHORITY\SYSTEM' -runLevel highest -LogonType ServiceAccount 
+$principal=new-scheduledtaskprincipal -userid 'Student\Admin' -runLevel highest 
 Register-ScheduledTask RegGitCredMgr -Action $Action -trigger $Trigger -Principal $principal 
