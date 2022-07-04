e42cbe7741e6435c596af44078886bb7cd4a4d2b
Student@DDLS <Student@DDLS>
2022-07-04 16:23:04 -0700

    Add enum demo

---
+++ MarksCode/Module-03-Basic CSharp Constructs/Demos/00-BasicEnums/00-BasicEnums/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00_BasicEnums
+{
+    class Program
+    {
+        enum WeekDay { Sun, Mon, Tue, Wed, Thu, Fri, Sat };
+        static void Main(string[] args)
+        {
+            int dayOfWeek;
+
+            dayOfWeek = 0; //Sunday
+            dayOfWeek = 1; //Monday
+
+            WeekDay holiday;
+            holiday = WeekDay.Mon;
+
+            holiday = holiday + 1;
+
+            Console.WriteLine($"the holiday will be {holiday}");
+            Console.ReadLine();
+
+
+        }
+    }
+}
