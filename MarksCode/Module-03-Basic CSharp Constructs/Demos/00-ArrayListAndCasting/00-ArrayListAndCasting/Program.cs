742a565dd807438bc25b9717982521772aec92e1
Student@DDLS <Student@DDLS>
2022-07-04 17:55:34 -0700

    Add m3 examples

---
+++ MarksCode/Module-03-Basic CSharp Constructs/Demos/00-ArrayListAndCasting/00-ArrayListAndCasting/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00_ArrayListAndCasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList items = new ArrayList();
+            items.Add(3);
+
+            //int result = items[0]; //this doesn't compile. why not? items[0] is an object, not an int
+
+            int result = (int)items[0]; //convert from object to int
+
+            Console.WriteLine($"result is {result}");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ MarksCode/Module-03-Basic CSharp Constructs/Demos/00-ListDictionary/00-ListDictionary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00_ListDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListDictionary myItems = new ListDictionary();
+
+            myItems.Add("96818", "Honalulu");
+            myItems.Add("77777", "New Gotbat");
+            myItems.Add("98877", "Upper tree");
+
+            Console.WriteLine(myItems["77777"]);
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
