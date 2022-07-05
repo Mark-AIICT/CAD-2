fde2f6108b39123eab904244b328a504acdbd3a1
Student@DDLS <Student@DDLS>
2022-07-04 17:33:24 -0700

    Add 00-ArrayList demo

---
+++ MarksCode/Module-03-Basic CSharp Constructs/Demos/00-ArrayList/00-ArrayList/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00_ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList listofthings = new ArrayList();
+            listofthings.Add("pencil");
+            listofthings.Add(23);
+            listofthings.Add(25.77M);
+
+            int itemNumber = 1;
+            foreach (var item in listofthings)
+            {
+                
+                Console.WriteLine($"item {itemNumber++} is {item}");
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
