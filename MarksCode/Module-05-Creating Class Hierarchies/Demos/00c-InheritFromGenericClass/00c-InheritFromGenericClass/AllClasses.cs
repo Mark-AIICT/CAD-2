c19fd64e1942be5236d6f550482c58a26c2f3baa
Student@DDLS <Student@DDLS>
2022-07-06 22:16:50 -0700

    Add decriptions to M5 demos

---
+++ MarksCode/Module-05-Creating Class Hierarchies/Demos/00c-InheritFromGenericClass/00c-InheritFromGenericClass/AllClasses.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00c_InheritFromGenericClass
+{
+    class TownList : List<Town>
+    {
+        public void Add(string townName, int zipCode )
+        {
+            this.Add(new Town() { ZipCode = zipCode, TownName = townName });
+        }
+
+        public string LastTown()
+        {
+            return this.Last().TownName;
+        }
+    }
+
+    class Town
+    {
+        public int ZipCode { get; set; }
+        public string TownName { get; set; }
+    }
+}
---
+++ MarksCode/Module-05-Creating Class Hierarchies/Demos/00c-InheritFromGenericClass/00c-InheritFromGenericClass/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00c_InheritFromGenericClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TownList towns = new TownList();
+            towns.Add("Banana",11111);
+            towns.Add("Woollongong",66666);
+            towns.Add("Coogee", 4444 );
+
+
+            Console.WriteLine($"The last town is {towns.LastTown()}\n\n");
+
+            foreach (var item in towns)
+            {
+                Console.WriteLine($"{item.TownName}, {item.ZipCode}");
+            }
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
