f7e99027b9153a67581cb5dd02317403953675ea
Student@DDLS <Student@DDLS>
2022-07-11 00:05:06 -0700

    Add Package examples to M6

---
+++ MarksCode/Module-06-Reading and Writing Local Data/Demos/XXa-DrawRedLine/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bitmap bmp = new Bitmap(400, 400);
+            Pen p = new Pen(Color.Red);
+            p.Width = 10;
+
+            Graphics gh = Graphics.FromImage(bmp);
+            gh.DrawLine(p, 0, 0, 100, 100);
+            bmp.Save("C:\\temp\\mydrawing.bmp");
+        }
+    }
+}
---
+++ MarksCode/Module-06-Reading and Writing Local Data/Demos/XXb-UseNewtonsoftJson/ConsoleApp2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Person> people = new List<Person>()
+            {
+                new Person() { PersonCity = "Perth", PersonName = "Rusty" },
+                new Person() { PersonCity = "Llilongwe", PersonName = "Trang" },
+                new Person() { PersonCity = "Melbourne", PersonName = "Mohammad" }
+            };
+
+            string serializedData = JsonConvert.SerializeObject(people, Formatting.Indented);
+            Console.WriteLine(serializedData);
+
+            Console.WriteLine("______________________________");
+
+
+            var p = JsonConvert.DeserializeObject<List<Person>>(serializedData);
+
+            foreach (var item in p)
+            {
+                Console.WriteLine($"{item.PersonName} is from {item.PersonCity}" );
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+
+    class Person
+    {
+        public string PersonName { get; set; }
+        public string PersonCity { get; set; }
+    }
+}
