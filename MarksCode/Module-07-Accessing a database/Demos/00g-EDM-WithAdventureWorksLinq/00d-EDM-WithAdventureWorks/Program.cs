1914a4aa51bf24dc061696728830407f57181a58
Student@DDLS <Student@DDLS>
2022-07-11 21:48:36 -0700

    Update M7 demos

---
+++ MarksCode/Module-07-Accessing a database/Demos/00g-EDM-WithAdventureWorksLinq/00d-EDM-WithAdventureWorks/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00d_EDM_WithAdventureWorks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AdventureWorksEntities ctx = new AdventureWorksEntities();
+
+
+            var avgProductprice = ctx.Products.Average(prod => prod.ListPrice);
+
+            Console.WriteLine($"The average product price in the database is {avgProductprice:C}");
+            Console.WriteLine("Press a key to end");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MarksCode/Module-07-Accessing a database/Demos/00h-ForceQueryExecution/00d-EDM-WithAdventureWorks/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00d_EDM_WithAdventureWorks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AdventureWorksEntities ctx = new AdventureWorksEntities();
+            Console.WriteLine("Type something that should be in the product Name");
+            var chars = Console.ReadLine();
+
+
+            var x_Products = (from p in ctx.Products
+                             where p.Name.Contains(chars)
+                             select new { Prodname = p.Name, Price = p.ListPrice }).ToList(); //ToList causes the query to run here
+
+            chars = "x";
+
+            foreach (var prd in x_Products)   //linq query runs here
+            {
+                Console.WriteLine($"{prd.Prodname}\t\t\t{prd.Price:C}");
+            }
+
+            Console.WriteLine("Press a key to end");
+            Console.ReadKey();
+        }
+    }
+}
