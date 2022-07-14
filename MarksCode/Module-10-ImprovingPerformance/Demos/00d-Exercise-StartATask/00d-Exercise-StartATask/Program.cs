07e54706258487b8d0004dacfa3e532632684ce6
Student@DDLS <Student@DDLS>
2022-07-13 21:20:02 -0700

    Update m10 demos

---
+++ MarksCode/Module-10-ImprovingPerformance/Demos/00d-Exercise-StartATask/00d-Exercise-StartATask/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00d_Exercise_StartATask
+{
+    delegate string MyDelegate(List<string> s);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "The ";
+            string s2 = "psychopathic ";
+            string s3 = "leader ";
+
+            List<string> strings = new List<string> { s1, s2, s3 };
+
+            MyDelegate pointerToMyJoiningFunction = JoinStrings;
+
+            Task<string> T = new Task<string>(new Func<List<string>,string>>(JoinStrings), strings);
+
+
+            T.Start();
+            T.Wait();
+
+            Console.WriteLine($"{T.Result}");
+
+            Console.WriteLine("Ok, we're done. press <enter> to end");
+            Console.ReadLine();
+            }
+
+        static string JoinStrings(List<string> x)
+        {
+
+            string result = "";
+            foreach (var item in x)
+            {
+                result += item;
+            }
+            return result;
+        }
+    
+
+        private static void DoSomethingThatTakesALongTime()
+        {
+            decimal result = 0;
+            for (decimal i = 0; i < 30000000M; i++)
+            {
+                result += i;
+            }
+        }
+    }
+
+}
+
---
+++ MarksCode/Module-10-ImprovingPerformance/Demos/00e-Exercise-StartATask/00d-Exercise-StartATask/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00d_Exercise_StartATask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "The ";
+            string s2 = "psychopathic ";
+            string s3 = "leader ";
+            string s4 = "keeps saying he did nothing wrong ";
+
+
+            List<string> strings = new List<string> { s1, s2, s3, s4 };
+
+            Task<string> T = new Task<string>(p =>
+            {
+                DoSomethingThatTakesALongTime();
+                string result = "";
+                foreach (var s in (List<string>)p)
+                {
+                    result += s;
+                }
+                return result;
+            }, strings);
+
+
+            T.Start();
+            Console.WriteLine("Processing task in background. Please wait.....");
+            T.Wait();
+
+            Console.WriteLine($"{T.Result}");
+
+            Console.WriteLine("Ok, we're done. press <enter> to end");
+            Console.ReadLine();
+            }
+    
+
+        private static void DoSomethingThatTakesALongTime()
+        {
+            decimal result = 0;
+            for (decimal i = 0; i < 30000000M; i++)
+            {
+                result += i;
+            }
+        }
+    }
+
+}
+
