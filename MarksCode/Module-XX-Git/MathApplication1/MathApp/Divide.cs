3957b728d3ec2c28ca041998a40b11d1545eaa0e
Mark Walsh <Mark Walsh@ddls.com.au>
2022-07-19 08:28:05 +1000

    Add Git App for exercise

---
+++ MarksCode/Module-XX-Git/MathApplication1/MathApp/Divide.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathApp
+{
+    partial class Program
+    {
+        public static decimal  Divide(decimal x, decimal y)
+        {
+            throw new NotImplementedException("This function has not been implemented yet");
+        }
+    }
+}
---
+++ MarksCode/Module-XX-Git/MathApplication1/MathApp/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathApp
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal x = 33, y = 4;
+            try
+            {
+                Console.WriteLine("\n\nAdd");
+                Console.WriteLine(Add(x, y));
+            }
+            catch (NotImplementedException ex)
+            {
+
+                Console.WriteLine(ex.Message); 
+            }
+
+            try
+            {
+                Console.WriteLine("\n\nSubtract");
+                Console.WriteLine(Subtract(x, y));
+            }
+            catch (NotImplementedException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                Console.WriteLine("\n\nDivide");
+                Console.WriteLine(Divide(x, y));
+            }
+            catch (NotImplementedException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                Console.WriteLine("\n\nMultiply");
+                Console.WriteLine(Multiply(x, y));
+            }
+            catch (NotImplementedException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                Console.WriteLine("\n\nCube");
+                Console.WriteLine(Cube(x, y));
+            }
+            catch (NotImplementedException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                Console.WriteLine("\n\nSquare");
+                Console.WriteLine(Square(x, y));
+            }
+            catch (NotImplementedException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
