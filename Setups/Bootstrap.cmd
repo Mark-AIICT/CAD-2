9b87749995945ace48dfbee41ddffbf042685c65
Mark-AIICT <105835753+Mark-AIICT@users.noreply.github.com>
2022-10-04 06:18:59 +1100

    Add Docker Desktop

--- Setups/Bootstrap.cmd
+++ Setups/Bootstrap.cmd
@@ -50,6 +50,7 @@ choco install notepadplusplus -y
 choco install fiddler -y
 choco install postman -y
 choco install nuget.commandline -y
+choco install docker-desktop -y
 
 REM I found trying to install Edge Chromium using Chocolatey was problematic. Giving Google Chrome a go.
 REM Download Edge Chromium (the versions can be found at https://edgeupdates.microsoft.com/api/products?view=enterprise...I used postman to get the JSON
